19e35a0e3a17eb6ca865449d5f808880ea9bb0cd
VandycKing <mygitglobal@gmail.com>
2024-01-25 06:33:46 +0000

    Bugfixes networking module source

--- aks-terraform/main.tf
+++ aks-terraform/main.tf
@@ -27,3 +27,23 @@ module "networking" {
   # Define more input variables as needed...
 }
 
+module "aks_cluster" {
+  source = "./aks-cluster-module"
+  # Input variables for the AKS cluster module
+  aks_cluster_name                = "terraform-aks-cluster"
+  cluster_location                = "UK South"
+  dns_prefix                      = "myaks-project"
+  kubernetes_version              = "1.26.6" # Adjust the version as needed
+  service_principal_client_id     = var.client_id
+  service_principal_client_secret = var.client_secret
+
+  # Input variables referencing outputs from the networking module
+  resource_group_name     = module.networking.resource_group_name
+  vnet_id                 = module.networking.vnet_id
+  control_plane_subnet_id = module.networking.control_plane_subnet_id
+  worker_node_subnet_id   = module.networking.worker_node_subnet_id
+  aks_nsg_id              = module.networking.aks_nsg_id
+
+  # Define more input variables as needed...
+}
+
